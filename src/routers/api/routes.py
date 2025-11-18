ac4528f2a7e1e1fa903569e30574292adeac5593
sofusl90 <sofus@laubel.net>
2025-11-18 14:17:04 +0100

    fix

--- src/routers/api/routes.py
+++ src/routers/api/routes.py
@@ -12,15 +12,15 @@ class StatusResponse(BaseModel):
 
 
 @router.get("/status", response_model=StatusResponse)
-def get_status(queue_name: str = Query(..., description="Queue name (solver_controller_id)")):
+def get_status(
+    queue_name: str = Query(..., description="Queue name (solver_controller_id)"),
+):
     """Retrieve messages from RabbitMQ queue"""
     messages = []
 
     credentials = pika.PlainCredentials(Config.RabbitMQ.USER, Config.RabbitMQ.PASSWORD)
     parameters = pika.ConnectionParameters(
-        host=Config.RabbitMQ.HOST,
-        port=Config.RabbitMQ.PORT,
-        credentials=credentials
+        host=Config.RabbitMQ.HOST, port=Config.RabbitMQ.PORT, credentials=credentials
     )
 
     connection = pika.BlockingConnection(parameters)
@@ -31,9 +31,11 @@ def get_status(queue_name: str = Query(..., description="Queue name (solver_cont
 
     # Get all messages from queue (non-blocking)
     while True:
-        method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
+        method_frame, header_frame, body = channel.basic_get(
+            queue=queue_name, auto_ack=True
+        )
         if method_frame:
-            messages.append(body.decode('utf-8'))
+            messages.append(body.decode("utf-8"))
         else:
             break
 
@@ -41,5 +43,5 @@ def get_status(queue_name: str = Query(..., description="Queue name (solver_cont
 
     return StatusResponse(
         isFinished=len(messages) == 0,  # Finished if no more messages
-        messages=messages
+        messages=messages,
     )
